66af9836c109d18c29a852e38c5709a1460106db
Rasmus Schultz <rasmus@mindplay.dk>
2011-06-01 02:06:22 +0000

    move standard annotations into namespace-folder

--- lib/Annotation/Standard/DisplayAnnotation.php
+++ lib/Annotation/Standard/DisplayAnnotation.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * This file is part of the php-annotation framework.
+ *
+ * (c) Rasmus Schultz <rasmus@mindplay.dk>
+ * 
+ * This software is licensed under the GNU LGPL license
+ * for more information, please see: 
+ * 
+ * <http://code.google.com/p/php-annotations>
+ */
+
+namespace Annotation\Lib;
+
+use Annotation\Annotation;
+
 /**
  * Defines various display-related metadata.
  */
---
+++ lib/Annotation/Standard/ParamAnnotation.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the php-annotation framework.
+ *
+ * (c) Rasmus Schultz <rasmus@mindplay.dk>
+ * 
+ * This software is licensed under the GNU LGPL license
+ * for more information, please see: 
+ * 
+ * <http://code.google.com/p/php-annotations>
+ */
+
+namespace Annotation\Standard;
+
+use Annotation\Annotation;
+use Annotation\IAnnotationParser;
+
+/**
+ * Defines a method-parameter's type
+ */
+class ParamAnnotation extends Annotation implements IAnnotationParser
+{
+  /**
+   * Parse the standard PHP-DOC @var annotation
+   */
+  public static function parseAnnotation($value)
+  {
+    return array();
+  }
+  
+  public function initAnnotation($properties)
+  {}
+}
