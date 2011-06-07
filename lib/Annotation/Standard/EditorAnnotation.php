7df70be87a1e1188a70245961e19f6760578f1fc
Rasmus Schultz <rasmus@mindplay.dk>
2011-06-07 00:04:56 +0000

    add notes

--- lib/Annotation/Standard/EditorAnnotation.php
+++ lib/Annotation/Standard/EditorAnnotation.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * This file is part of the php-annotation framework.
+ *
+ * (c) Rasmus Schultz <rasmus@mindplay.dk>
+ * 
+ * This software is licensed under the GNU LGPL license
+ * for more information, please see: 
+ * 
+ * <http://code.google.com/p/php-annotations>
+ */
+
+namespace Annotation\Lib;
+
 /**
  * Specifies the name of a view to use for rendering an input on a form.
  */
--- lib/Annotation/Standard/FormatAnnotation.php
+++ lib/Annotation/Standard/FormatAnnotation.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * This file is part of the php-annotation framework.
+ *
+ * (c) Rasmus Schultz <rasmus@mindplay.dk>
+ * 
+ * This software is licensed under the GNU LGPL license
+ * for more information, please see: 
+ * 
+ * <http://code.google.com/p/php-annotations>
+ */
+
+namespace Annotation\Lib;
+
 /**
  * Specifies how to display or format a property value (for display-purposes).
  */
