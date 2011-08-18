14d7a6ef4b0f653db7ef065682ff2fac29f1838e
Luís <lcobucci@gmail.com>
2011-08-18 15:34:58 -0300

    Interface not found...

--- lib/Annotation/Standard/EditableAnnotation.php
+++ lib/Annotation/Standard/EditableAnnotation.php
@@ -4,10 +4,10 @@
  * This file is part of the php-annotation framework.
  *
  * (c) Rasmus Schultz <rasmus@mindplay.dk>
- * 
+ *
  * This software is licensed under the GNU LGPL license
- * for more information, please see: 
- * 
+ * for more information, please see:
+ *
  * <http://code.google.com/p/php-annotations>
  */
 
@@ -18,26 +18,26 @@ namespace Annotation\Lib;
  *
  * @usage('property'=>true, 'inherited'=>true)
  */
-class EditableAnnotation extends Annotation implements IDataAnnotation
+class EditableAnnotation extends Annotation
 {
   /**
    * @var bool Indicates whether or not a property is editable.
    */
   public $allow=false;
-  
+
   /**
    * @var bool Indicates whether or not a property is editable on a new instance.
    *           (this value only has meaning when $allow is false.)
    */
   public $first=false;
-  
+
   /**
    * Initialize the annotation.
    */
   public function initAnnotation($properties)
   {
     $this->_map($properties, array('allow', 'first'));
-    
+
     parent::initAnnotation($properties);
   }
 }
