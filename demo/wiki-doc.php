cc287b3485d1893cbb511cf383e11f265023a6ed
Alexander Obuhovich <aik.bold@gmail.com>
2014-02-15 21:03:12 +0200

    Fixed demo manual generation after "index.php" line ending change

--- demo/wiki-doc.php
+++ demo/wiki-doc.php
@@ -4,37 +4,32 @@
 
 header('Content-type: text/plain');
 
-$file = file_get_contents('index.php');
-
 echo '<table border="0" cellpadding="0" cellspacing="0">' . "\n";
 echo '<tr><td valign="top" width="300">' . "\n";
 echo "<h3>demo/index.php</h3>\n";
 
 $code = false;
 
-foreach (explode("\n", $file) as $line)
-{
-  if (substr(ltrim($line),0,2) == '##')
-  {
-    if ($code)
-    {
-      echo "</pre>\n";
-      echo "</td></tr>";
-      echo '<tr><td valign="top"><br/>' . "\n";
-      $code = false;
+foreach (file('index.php') as $line) {
+    if (substr(ltrim($line), 0, 2) == '##') {
+        if ($code) {
+            echo "</pre>\n";
+            echo "</td></tr>";
+            echo '<tr><td valign="top"><br/>' . "\n";
+            $code = false;
+        }
+
+        echo htmlspecialchars(rtrim(substr(ltrim($line), 3))) . "\n";
+    } else {
+        // it's code
+        if (!$code) {
+            echo '</td><td valign="top">' . "\n";
+            echo "<pre>";
+            $code = true;
+        }
+
+        echo htmlspecialchars($line);
     }
-    echo htmlspecialchars(rtrim(substr(ltrim($line),3))) . "\n";
-  }
-  else // it's code
-  {
-    if (!$code)
-    {
-      echo '</td><td valign="top">'."\n";
-      echo "<pre>";
-      $code = true;
-    }
-    echo htmlspecialchars($line);
-  }
 }
 
 echo '</table>';
