d7243b1ceef8d3e59e07934a596efde5756eab19
Alexander Obuhovich <aik.bold@gmail.com>
2015-01-31 15:07:35 +0200

    Initial documentation version

--- demo/wiki-doc.php
+++ demo/wiki-doc.php
@@ -1,35 +1,32 @@
 <?php
 
-## This script generates formatted documentation for the demo, in Wiki format.
+## This script generates formatted documentation for the demo, in RST format.
 
 header('Content-type: text/plain');
+echo 'File: **demo/index.php**' . PHP_EOL;
 
-echo '<table border="0" cellpadding="0" cellspacing="0">' . "\n";
-echo '<tr><td valign="top" width="300">' . "\n";
-echo "<h3>demo/index.php</h3>\n";
+$codeStart = 1;
 
-$code = false;
-
-foreach (file('index.php') as $line) {
+foreach (file('index.php') as $lineNumber => $line) {
     if (substr(ltrim($line), 0, 2) == '##') {
-        if ($code) {
-            echo "</pre>\n";
-            echo "</td></tr>";
-            echo '<tr><td valign="top"><br/>' . "\n";
-            $code = false;
+        if ($codeStart !== false) {
+            echo PHP_EOL;
+            echo '.. literalinclude:: ../demo/index.php' . PHP_EOL;
+            echo '   :lines: ' . ($codeStart + 1) . '-' . $lineNumber . PHP_EOL;
+            echo PHP_EOL;
+            $codeStart = false;
         }
 
-        echo htmlspecialchars(rtrim(substr(ltrim($line), 3))) . "\n";
+        echo str_replace('`', '``', rtrim(substr(ltrim($line), 3))) . PHP_EOL;
     } else {
         // it's code
-        if (!$code) {
-            echo '</td><td valign="top">' . "\n";
-            echo "<pre>";
-            $code = true;
+        if ($codeStart === false) {
+            $codeStart = $lineNumber;
         }
-
-        echo htmlspecialchars($line);
     }
 }
 
-echo '</table>';
+echo PHP_EOL;
+echo '.. literalinclude:: ../demo/index.php' . PHP_EOL;
+echo '   :lines: ' . ($codeStart + 1) . '-' . ($lineNumber + 1) . PHP_EOL;
+echo PHP_EOL;
